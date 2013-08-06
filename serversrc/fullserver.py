2a88d0f0a664877ac5b286d842bb6c35eab3204d
Adam Keim <adam.frusa@gmail.com>
2013-08-06 08:45:39 -0500

    Tkinter build (not funtional)
    Only commiting so that I can have a backup and see my old code later

--- serversrc/fullserver.py
+++ serversrc/fullserver.py
@@ -1,34 +1,44 @@
-
-
+from Tkinter import *
+import tkMessageBox
+from multiprocessing import Process
 import socket, string, select, sys
-HOST, PORT = '', 5007
+root = Tk()
+def quitnow():
+    if tkMessageBox.askokcancel("Quit", "Are you sure you want to terminate the PyChat server"):
+        root.destroy()
 
+HOST, PORT = '', 5007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-s.bind((HOST, PORT))
-s.listen(1)
-try:
-    f = open('log.txt', 'a')
-    print 'logfile detected'
-    f.close()
-except:
-    print "Will not use logfile"
 socknames = {}
-def write(message):
-    
-    global f
+
+def start():
+    s.bind((HOST, PORT))
+    s.listen(1)
     try:
         f = open('log.txt', 'a')
-        f.write(message)
+        print 'logfile detected'
         f.close()
-    except: pass
+    except:
+        print "Will not use logfile"
+        
+def write(message):
+    
+        global f
+        try:
+            f = open('log.txt', 'a')
+            f.write(message)
+            f.close()
+        except: pass
+        
 def broadcast(message, sender):
-    write(message)
-    for sock in socknames.keys():
-        if sock is not sender:
-            sock.send(message)
+        write(message)
+        for sock in socknames.keys():
+            if sock is not sender:
+                sock.send(message)
+                
+def loop():
 
-while 1:
     nums = [s.fileno()]
     for sock in socknames.keys():
         nums.append(sock.fileno())
@@ -62,4 +72,13 @@ while 1:
                 if text == '/killserver':
                     sys.exit(0)
                 broadcast("%s> %s\n" % (name, text), sock)
+    root.after(100, loop)
 
+
+root.title('Chat Server Panel')
+Label(text="Basic Commands").grid(row=0, column=0, columnspan=3)
+Button( text='Start', command=start() ) .grid(row=1, column=0, padx=5, pady=5)
+Button( text='Stop') .grid(row=1, column=1, padx=5, pady=5)
+Button( text='Quit', command=quitnow()) .grid(row=1, column=2, padx=5, pady=5)   
+root.after(2000, loop)
+root.mainloop()
